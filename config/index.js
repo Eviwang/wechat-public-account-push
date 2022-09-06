952c7702a32b5073e3efdfc14428bb241d9c95b1
EviWang <525205537@qq.com>
2022-09-07 00:07:12 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ export const config = {
       // 所在城市
       city: "长沙",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: ""
+      openUrl: "https://weibo.com/hot/search"
     }
   ],
 
@@ -50,7 +50,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "KYLOMfenI-tqjBDu2sDyf2wpA-s95NNPRpUfx_lTvzU",
+  CALLBACK_TEMPLATE_ID: "npkrLv9zafQ1OUvibjJMX-1fTjKgFAh00cADr1laYww",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -99,10 +99,8 @@ export const config = {
     * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }} 
     * */
   CUSTOMIZED_DATE_LIST: [
-    // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
-//     // 结婚纪念日
-//     {"keyword": "marry_day", date: "2020-01-04"},
+    // 测试
+    {"keyword": "love_day", date: "2021-11-27"},
 //     // 退伍日, 不用可以删掉
 //     {"keyword": "ex_day", date: "2022-08-31"},
     // sakana日
